5797ff6a65e098c3feb3c03ea618aa4704cb60c8
Sargis Mkhitaryan <sargismk@icloud.com>
2018-10-03 13:52:58 +0400

    Add Person Entity.

---
+++ CoreDataExample/Model/PersonStack.swift
@@ -0,0 +1,19 @@
+//
+//  PersonStack.swift
+//  CoreDataExample
+//
+//  Created by Sargis Mkhitaryan on 10/3/18.
+//  Copyright © 2018 Sargis. All rights reserved.
+//
+
+import CoreData
+
+func createPeopleContainer(completion: @escaping (NSPersistentContainer) -> ()) {
+    let container = NSPersistentContainer(name: "PeopleModel")
+    container.loadPersistentStores { (_, error) in
+        guard error == nil else {
+            fatalError("Failed to load store. Error: \(error!)")
+        }
+        completion(container)
+    }
+}
